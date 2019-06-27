a902b349a2a3ad6768925bc8a524652c512a1dba
Liuym <liuym@startimes.com.cn>
2019-06-27 13:25:56 +0800

    jersey 引入

--- service/authorization/authorization-service/src/main/java/com/sydml/authorization/service/IUserService.java
+++ service/authorization/authorization-service/src/main/java/com/sydml/authorization/service/IUserService.java
@@ -5,12 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
 /**
  * @author Liuym
  * @date 2019/3/25 0025
  */
+@Path("user")
 public interface IUserService {
 
+    @GET
     UserDTO findByUsername(@RequestParam(value = "username") String username);
 
     UserDTO save(UserDTO userDTO);
