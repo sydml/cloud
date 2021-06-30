f3ec6fca660d792b49b1cd592e7cb0730224eb2b
liuyuming11 <liuyuming11@jd.com>
2021-06-30 20:16:36 +0800

    限流测试

--- platform/platform-gateway-server/src/test/java/com/sydml/gateway/GatewayApplicationTests.java
+++ platform/platform-gateway-server/src/test/java/com/sydml/gateway/GatewayApplicationTests.java
@@ -1,16 +1,29 @@
-//package com.sydml.gateway;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class GatewayApplicationTests {
-//
-//    @Test
-//    public void contextLoads() {
-//    }
-//
-//}
+package com.sydml.gateway;
+
+import com.sydml.common.utils.JsonUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GatewayApplicationTests {
+    @Test
+    public void contextLoads() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("token", "test");
+        headers.add("LIMIT_KEY", "redisRequestLimtKey");
+        for (int i = 0; i < 1000; i++) {
+            ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:9000/api-authorization/redis/test", String.class);
+            System.out.println("限流返回结果："+JsonUtil.encodeJson(forEntity));
+        }
+
+    }
+
+}
