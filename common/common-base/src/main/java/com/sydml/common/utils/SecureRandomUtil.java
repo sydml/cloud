900e0a827db641777028d781ca2eba5e995aa7d9
liuyuming11 <liuyuming11@jd.com>
2021-07-01 18:22:45 +0800

    随机数生成重构

--- common/common-base/src/main/java/com/sydml/common/utils/SecureRandomUtil.java
+++ common/common-base/src/main/java/com/sydml/common/utils/SecureRandomUtil.java
@@ -1,13 +1,18 @@
 package com.sydml.common.utils;
 
 import java.security.SecureRandom;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author liuyuming
  * @date 2021-06-08 10:41:26
+ * SecureRandom 同步，性能较差，线程安全，可以指定算法，比Random性能还差
+ * Random 线程安全，同步，使用系统时间纳秒作为种子
+ * ThreadLocalRandom 线程安全，并发，使用系统时间纳秒作为种子
  */
 public class SecureRandomUtil {
     public static SecureRandom random = new SecureRandom();
+    public static ThreadLocalRandom threadLocalRandom = ThreadLocalRandom.current();
 
     public static String getRandom(int length) {
         StringBuilder ret = new StringBuilder();
@@ -21,5 +26,14 @@ public class SecureRandomUtil {
             }
         }
         return ret.toString();
+    }
+
+    /**
+     * 生成指定长度的随机数
+     * @param length
+     * @return
+     */
+    public static int getSpecifiedLength(int length) {
+        return threadLocalRandom.nextInt((int)Math.pow(10, length - 1), (int)Math.pow(10, length));
     }
 }
